2bd83d9aebff03e767ac09089b7904e673a3a6f3
Kevo <kevo548@gmail.com>
2016-06-15 02:16:32 -0400

    messin with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,28 @@
 /*
- * Created by Kevo on 6/9/2016.
+ * Created by Kevo on 6/13/2016.
  */
 
 const gulp         = require("gulp"),
-      gutil        = require("gulp-util"),
-      gulpif       = require("gulp-if"),
-      autoprefixer = require("gulp-autoprefixer"),
-      cssmin       = require("gulp-cssmin"),
-      less         = require("gulp-less"),
-      concat       = require("gulp-concat"),
-      plumber      = require("gulp-plumber"),
-      buffer       = require("vinyl-buffer"),
-      source       = require("vinyl-source-stream"),
-      babelify     = require("babelify"),
-      browserify   = require("browserify"),
-      watchify     = require("watchify"),
-      uglify       = require("gulp-uglify"),
-      sourcemaps   = require("gulp-sourcemaps"),
-      eslint       = require("gulp-eslint");
+	gutil        = require("gulp-util"),
+	gulpif       = require("gulp-if"),
+	autoprefixer = require("gulp-autoprefixer"),
+	cssmin       = require("gulp-cssmin"),
+	less         = require("gulp-less"),
+	concat       = require("gulp-concat"),
+	plumber      = require("gulp-plumber"),
+	buffer       = require("vinyl-buffer"),
+	source       = require("vinyl-source-stream"),
+	babelify     = require("babelify"),
+	browserify   = require("browserify"),
+	watchify     = require("watchify"),
+	uglify       = require("gulp-uglify"),
+	sourcemaps   = require("gulp-sourcemaps"),
+	eslint       = require("gulp-eslint");
 
 const logger = require("./cfgs/logger"),
-      fs     = require("fs"),
-      moment = require("moment"),
-      now    = moment().format();
+	fs     = require("fs"),
+	moment = require("moment"),
+	now    = moment().format();
 
 const production = process.env.NODE_ENV === "production";
 
@@ -39,20 +39,37 @@ gutil.log("Gulp Started!");
 
 /*
  |--------------------------------------------------------------------------
- | Check if Winston logfile exists, if not then create it. also a watch
+ | Check/Watch if Winston logfolder/file exists, if not then create it
  |--------------------------------------------------------------------------
  */
+
+// gulp.task("logs1", function() {
+// 	fs.stat("logs", function(err, stat) {
+// 		if (err == null) {
+// 			gutil.log("LogFolder Exists");
+// 		}
+//
+// 		else {
+// 			gutil.log("LogFolder Does Not Exist, Creating Now");
+// 			fs.mkdir("logs", { mode: "666" });
+// 			gutil.log("LogFolder Created!");
+// 		}
+// 	});
+// });
+
 // currently watches for files changes, make it so it only watches for a delete? also mkdir for logs if doesn't exist
 gulp.task("logs", function() {
 	fs.stat("logs/all-logs.log", function(err, stat) {
-		if(err == null) {
+		if (err == null) {
 			gutil.log("LogFile Exists");
 		}
 
 		else {
+
 			gutil.log("LogFile Does Not Exist, Creating Now");
-			fs.writeFile("logs/all-logs.log", "LogFile Created @ " + now + "\r\n", {mode: "666"});
+			fs.writeFile("logs/all-logs.log", "LogFile Created @ " + now + "\r\n", { mode: "666" });
 			gutil.log("LogFile Created!");
+
 		}
 	});
 });
@@ -154,5 +171,5 @@ gulp.task("watch", function() {
 	gulp.watch("logs/**/*.log", ["logs"]);
 });
 
-gulp.task("default", ["logs", "styles", "vendor", "browserify-watch", "watch"]);
-gulp.task("build", ["logs", "styles", "vendor", "browserify"]);
+gulp.task("default", ["styles", "logs", "vendor", "browserify-watch", "watch"]);
+gulp.task("build", ["styles", "logs", "vendor", "browserify"]);
